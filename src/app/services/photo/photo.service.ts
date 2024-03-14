3195fb4cd7c43b48aa9735d12fd31073fa9e4f39
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-03-14 16:14:21 -0500

    Las fotos tomadas se visualizan en una matriz

--- src/app/services/photo/photo.service.ts
+++ src/app/services/photo/photo.service.ts
@@ -3,10 +3,16 @@ import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
+  public photos: UserPhoto[] = [];
 
   constructor() { }
 
@@ -16,6 +22,11 @@ export class PhotoService {
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
+    });
+    
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath!
     });
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,31 @@
 import { camera } from 'ionicons/icons'
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { addIcons } from 'ionicons';
 import { PhotoService } from '../services/photo/photo.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab, IonFabButton, IonIcon]
+  imports: [
+    IonImg,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    NgFor
+  ]
 })
 export class Tab2Page {
 
